db38ed83b9e480800aa9d7b74dce364d0738667f
evankhan0123 <evankhan1234@gmail.com>
2020-06-25 21:22:55 -0700

    ok

--- app/src/main/java/com/nextgenit/doctor/Adapter/DashboardAdapter.java
+++ app/src/main/java/com/nextgenit/doctor/Adapter/DashboardAdapter.java
@@ -6,18 +6,14 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
-
 import com.nextgenit.doctor.R;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
